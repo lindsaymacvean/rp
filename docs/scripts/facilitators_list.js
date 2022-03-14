17da9a3803889e7bf4bea75dee38ccc787c35456
aromakh <andriyromakh@gmail.com>
2022-03-14 18:00:28 +0200

    refactoring

---
+++ docs/scripts/facilitators_list.js
@@ -0,0 +1,7 @@
+(function () {
+    axios.get('https://hzk5v7sv55.execute-api.eu-west-1.amazonaws.com/ReadableApiStage/facilitator/list')
+        .then(resp => {
+            var template = Handlebars.compile(document.querySelector("#facilitatorTemplate").innerHTML);
+            document.querySelector("#facilitatorsList").innerHTML = template({facilitators: resp.data.Items});
+        })
+})();
