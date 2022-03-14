5ab029c744c44fbdbf5c9de0634eb0d49b19a28b
aromakh <andriyromakh@gmail.com>
2022-03-14 20:08:25 +0200

    get facilitator details

--- docs/scripts/facilitator.js
+++ docs/scripts/facilitator.js
@@ -1,7 +1,11 @@
 (function () {
-    axios.get('https://hzk5v7sv55.execute-api.eu-west-1.amazonaws.com/ReadableApiStage/facilitator/list')
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    axios.get(`https://hzk5v7sv55.execute-api.eu-west-1.amazonaws.com/ReadableApiStage/facilitator?id=${id}`)
         .then(resp => {
-            var template = Handlebars.compile(document.querySelector("#facilitatorTemplate").innerHTML);
-            document.querySelector("#facilitatorsList").innerHTML = template(facilitators: resp.data.Items);
+            var template = Handlebars.compile(document.querySelector("#facilitatorDataTempalte").innerHTML);
+            document.querySelector("#facilitatorData").innerHTML = template(resp.data.Item);
         })
 })();
---
+++ src/getFacilitator.js
@@ -0,0 +1,35 @@
+let response;
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async(event, context) => {
+
+    try {
+
+        //TODO check if current user is Lead Facilitator.
+
+        var params = {
+            TableName: 'facilitator',
+            Key: {
+                'id': event.queryStringParameters.id
+            }
+        };
+
+        var body = await dynamo.get(params).promise();
+
+
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify(body),
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }
+
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
