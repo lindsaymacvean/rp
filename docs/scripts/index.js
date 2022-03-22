598bf0028474e1dad87a180dd99f9d3cfd0fb1fa
aromakh <andriyromakh@gmail.com>
2022-03-22 18:16:14 +0200

    fix

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -3,7 +3,8 @@
 
     const urlParams = new URLSearchParams(window.location.hash.substring(1));
     const id_token = urlParams.get('id_token');
-    sessionStorage.setItem('id_token', id_token);
+    if (id_token)
+        sessionStorage.setItem('id_token', id_token);
 
 
 })();
