ca10cf979b53fcf394d96c7a356aadb1e3bc0981
Andriy Romakh <andriyromakh@gmail.com>
2022-03-24 15:52:38 +0200

    auth redirect.

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,3 +1,5 @@
+const redable_google_auth_url = "https://dyslexia.auth.eu-west-1.amazoncognito.com/login?client_id=91933533mop1fsmepjklman27&response_type=token&scope=email+openid+profile&redirect_uri=https://lindsaymacvean.github.io/rp/";
+
 (function() {
     debugger;
 
@@ -5,6 +7,8 @@
     const id_token = urlParams.get('id_token');
     if (id_token)
         sessionStorage.setItem('id_token', id_token);
-
+    else {
+        window.location.href = redable_google_auth_url;
+    }
 
 })();
