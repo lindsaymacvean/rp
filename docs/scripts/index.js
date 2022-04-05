d1006eda7321816a9a9bb0a335e97172b1540f97
Andriy Romakh <andriyromakh@gmail.com>
2022-04-05 18:15:42 +0300

    google auth url fix

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,4 +1,5 @@
-const redable_google_auth_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&scope=email+openid+profile&redirect_uri=http://localhost:3000/docs/index.html";
+// const redable_google_auth_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&scope=email+openid+profile&redirect_uri=http://localhost:3000/docs/index.html";
+const redable_google_auth_url = "https://dyslexia.auth.eu-west-1.amazoncognito.com/login?client_id=91933533mop1fsmepjklman27&response_type=token&scope=email+openid+profile&redirect_uri=https://lindsaymacvean.github.io/rp/";
 
 (function() {
 
