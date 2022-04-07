2cb4987264ca5c113c561bedcedbd1f9869d29bf
Andriy Romakh <andriyromakh@gmail.com>
2022-04-07 21:51:32 +0300

    document search

--- docs/scripts/documents_list.js
+++ docs/scripts/documents_list.js
@@ -2,6 +2,25 @@ import { api_url } from "./utils/configs.js"
 
 (function() {
 
+    globalThis.search = (event) => {
+
+        clearTimeout(globalThis.searchTimeout);
+
+        globalThis.searchTimeout = setTimeout(() => {
+            axios.get(`${api_url}/documents?searchString=${document.getElementById("search_input").value}`, {
+                    headers: {
+                        'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
+                    }
+                })
+                .then(resp => {
+                    var template = Handlebars.compile(document.querySelector("#documentsTemplate").innerHTML);
+                    document.querySelector("#documentsList").innerHTML = template({ documents: resp.data.data.files });
+                })
+
+        }, 1000)
+
+    }
+
     axios.get(`${api_url}/documents`, {
             headers: {
                 'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
