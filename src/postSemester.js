d6cccfb6e9b968897c4859a69b6b01d61434d709
Andriy Romakh <andriyromakh@gmail.com>
2022-04-07 17:51:29 +0300

    info

--- src/postSemester.js
+++ src/postSemester.js
@@ -6,6 +6,9 @@ exports.handler = async(event, context, callback) => {
 
     var data = JSON.parse(event.body);
 
+    //TODO check if current user is Lead Facilitator.
+    //TODO validate data
+
     const item = {
         'name': data.name,
         'id': crypto.randomUUID()
