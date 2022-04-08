984a2289a1b09eeb2cb610ea160058481261a8dd
Andriy Romakh <andriyromakh@gmail.com>
2022-04-08 18:40:27 +0300

    config fix.

--- docs/scripts/utils/configs.js
+++ docs/scripts/utils/configs.js
@@ -1,7 +1,7 @@
-export const redable_google_auth_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&scope=email+openid+profile&redirect_uri=https://localhost";
-export const api_url = "https://g3z09urod0.execute-api.eu-west-2.amazonaws.com/ReadableApiStage"
-export const google_client_id = "558820778077-59ngbdg8dcht3cta26tmpblt05tqo3o5.apps.googleusercontent.com"
+// export const redable_google_auth_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&scope=email+openid+profile&redirect_uri=https://localhost";
+// export const api_url = "https://g3z09urod0.execute-api.eu-west-2.amazonaws.com/ReadableApiStage"
+// export const google_client_id = "558820778077-59ngbdg8dcht3cta26tmpblt05tqo3o5.apps.googleusercontent.com"
 
-// const redable_google_auth_url = "https://dyslexia.auth.eu-west-1.amazoncognito.com/login?client_id=91933533mop1fsmepjklman27&response_type=token&scope=email+openid+profile&redirect_uri=https://lindsaymacvean.github.io/rp/";
-// export const api_url = "https://hzk5v7sv55.execute-api.eu-west-1.amazonaws.com/ReadableApiStage"
-// export const google_client_id = "925322795697-i9jr2o8gkqan3cdqvkn7tjog5la33av6.apps.googleusercontent.com"
+export const redable_google_auth_url = "https://dyslexia.auth.eu-west-1.amazoncognito.com/login?client_id=91933533mop1fsmepjklman27&response_type=token&scope=email+openid+profile&redirect_uri=https://lindsaymacvean.github.io/rp/";
+export const api_url = "https://hzk5v7sv55.execute-api.eu-west-1.amazonaws.com/ReadableApiStage"
+export const google_client_id = "925322795697-i9jr2o8gkqan3cdqvkn7tjog5la33av6.apps.googleusercontent.com"
