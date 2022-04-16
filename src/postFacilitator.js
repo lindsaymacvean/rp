baeb3b3ff0ee106595b1203922f21967a8ec25c3
Lindsay MacVean <lindsaymacvean@gmail.com>
2022-04-16 01:04:46 +0100

    updated postFacilitator

--- src/postFacilitator.js
+++ src/postFacilitator.js
@@ -8,13 +8,9 @@ exports.handler = async(event, context, callback) => {
 
     console.log(data);
 
-    // var semester = (await getSemester(data.semesterId)).Item;
-    // var facilitator = (await getFacilitator(data.facilitatorId)).Item;
-    // //TODO if semester or facilitator is not found return error.
-
-    // var group = await createGroup(data);
-    // await addGroupToSemester(semester, group);
-    // await addGroupToFacilitator(facilitator, group);
+    // Check if it is a readable project entry (if not send email warning)
+    // First Check if a user already exists for this Email and PPS number
+    // Either insert or update the user
 
     response = {
         'statusCode': 200,
@@ -27,75 +23,45 @@ exports.handler = async(event, context, callback) => {
     return response;
 };
 
-const getSemester = async(semesterId) => {
-    var params = {
-        TableName: 'semester',
-        Key: {
-            'id': semesterId
-        }
-    };
-
-    return await dynamo.get(params).promise();
-};
-
-const getFacilitator = async(facilitatorId) => {
-    var params = {
-        TableName: 'facilitator',
-        Key: {
-            'id': facilitatorId
-        }
-    };
-
-    return await dynamo.get(params).promise();
-};
-
-const createGroup = async(data) => {
-    const item = {
-        'name': data.name,
-        'dayOfWeek': data.dayOfWeek,
-        'time': data.time,
-        'dateOfFirstSession': data.dateOfFirstSession,
-        'themes': data.themes,
-        'facilitatorId': data.facilitatorId,
-        'semesterId': data.semesterId,
-        'id': crypto.randomUUID()
-    }
-
-    var params = {
-        TableName: 'group',
-        Item: item
-    };
-
-    await dynamo.put(params).promise();
-    return item;
-};
-
-const addGroupToSemester = async(semester, group) => {
-
-    if (!semester.groupsIds)
-        semester.groupsIds = [];
-
-    semester.groupsIds.push(group.id);
-
-    var params = {
-        TableName: 'semester',
-        Item: semester
-    };
-
-    await dynamo.put(params).promise();
-};
-
-const addGroupToFacilitator = async(facilitator, group) => {
-
-    if (!facilitator.groupsIds)
-        facilitator.groupsIds = [];
-
-    facilitator.groupsIds.push(group.id);
-
-    var params = {
-        TableName: 'facilitator',
-        Item: facilitator
-    };
-
-    await dynamo.put(params).promise();
-};
+// The following is a payload from Google Form
+// 2022-04-15T18:28:26.232Z	43da3e03-d859-4f92-bb36-83c386d468df	INFO	{
+//     '397580085': {
+//       question: 'Upload your Tusla Children First Certificate                 ',
+//       answer: [ '1xoC9UY_Bpiv16Vtvh8okZ3kLQUKDJfSN' ]
+//     },
+//     '668240073': {
+//       question: 'Name & Surname of next of kin ',
+//       answer: 'Amelia norman'
+//     },
+//     '686485464': {
+//       question: 'Do you work in a Workshop or on the Readable Project? Please select',
+//       answer: 'Readable Project'
+//     },
+//     '723686850': { question: 'Date of Birth', answer: '1988-08-09' },
+//     '807410528': { question: 'EIRCODE', answer: 'test' },
+//     '862586168': { question: 'Surname', answer: 'macvean' },
+//     '1101563730': { question: 'First Name', answer: 'lindsay' },
+//     '1130390469': { question: 'Email Address', answer: 'lindsaymacvean@gmail.com' },
+//     '1163629015': { question: 'PPS Number', answer: '121212121c' },
+//     '1168939214': {
+//       question: 'Are you in full-time employment elsewhere, in addition to this part-time employment in a DAI Workshop/Readable Project?    ',
+//       answer: 'yes'
+//     },
+//     '1381149780': {
+//       question: 'Will you be working every week or subbing occasionally?',
+//       answer: 'every week'
+//     },
+//     '1481823688': { question: 'Address', answer: 'test' },
+//     '1510389555': { question: 'Mobile phone for next of kin', answer: '0877086920' },
+//     '1814285644': { question: 'Mobile Number', answer: '0877086920' },
+//     '1955092166': { question: 'Choose PRSI Contribution Class  ', answer: 'A' },
+//     '1980586813': {
+//       question: 'Enter the date on your DAI Vetting Disclosure notice showing your Garda Vetting is complete  ',
+//       answer: '2021-08-09'
+//     },
+//     '2002930037': {
+//       question: 'If you work in one of the Workshop please choose the location below from the drop down menu, if not, choose Not Applicable:',
+//       answer: 'Carlow: Tullow'
+//     }
+//   }
+  
