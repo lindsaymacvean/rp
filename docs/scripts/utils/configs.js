468985c5d13895750802e5ef36a714399267a7b1
aromakh <andriyromakh@gmail.com>
2022-04-21 14:01:11 +0300

    andrii configs fix.

--- docs/scripts/utils/configs.js
+++ docs/scripts/utils/configs.js
@@ -1,5 +1,5 @@
 // export const readable_cognito_auth_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&scope=email+openid+profile&redirect_uri=https://localhost";
-// export const readable_cognito_logout_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=91933533mop1fsmepjklman27&response_type=token&redirect_uri=https://localhost";
+// export const readable_cognito_logout_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&redirect_uri=https://localhost";
 // export const api_url = "https://g3z09urod0.execute-api.eu-west-2.amazonaws.com/ReadableApiStage"
 // export const google_client_id = "558820778077-59ngbdg8dcht3cta26tmpblt05tqo3o5.apps.googleusercontent.com"
 // export const frontend_url = "https://localhost"
