594fa91134a42a8e8589b22d5b58a4867f03dda1
aromakh <andriyromakh@gmail.com>
2022-04-22 14:23:37 +0300

    deploy script switch to new aws

--- docs/scripts/utils/configs.js
+++ docs/scripts/utils/configs.js
@@ -1,11 +1,11 @@
-export const readable_cognito_auth_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&scope=email+openid+profile&redirect_uri=https://localhost";
-export const readable_cognito_logout_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&redirect_uri=https://localhost";
-export const api_url = "https://g3z09urod0.execute-api.eu-west-2.amazonaws.com/ReadableApiStage"
-export const google_client_id = "558820778077-59ngbdg8dcht3cta26tmpblt05tqo3o5.apps.googleusercontent.com"
-export const frontend_url = "https://localhost"
+// export const readable_cognito_auth_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&scope=email+openid+profile&redirect_uri=https://localhost";
+// export const readable_cognito_logout_url = "https://aromakh.auth.eu-west-2.amazoncognito.com/login?client_id=443ahpd9l151fmj7b8h4df5qi5&response_type=token&redirect_uri=https://localhost";
+// export const api_url = "https://g3z09urod0.execute-api.eu-west-2.amazonaws.com/ReadableApiStage"
+// export const google_client_id = "558820778077-59ngbdg8dcht3cta26tmpblt05tqo3o5.apps.googleusercontent.com"
+// export const frontend_url = "https://localhost"
 
-// export const readable_cognito_auth_url = "https://dyslexia.auth.eu-west-1.amazoncognito.com/login?client_id=91933533mop1fsmepjklman27&response_type=token&scope=email+openid+profile&redirect_uri=https://rp.dyslexia.ie";
-// export const readable_cognito_logout_url = "https://dyslexia.auth.eu-west-1.amazoncognito.com/login?client_id=91933533mop1fsmepjklman27&response_type=token&redirect_uri=https://rp.dyslexia.ie";
-// export const api_url = "https://hzk5v7sv55.execute-api.eu-west-1.amazonaws.com/ReadableApiStage"
-// export const google_client_id = "925322795697-i9jr2o8gkqan3cdqvkn7tjog5la33av6.apps.googleusercontent.com"
-// export const frontend_url = "https://rp.dyslexia.ie"
+export const readable_cognito_auth_url = "https://readable.auth.eu-west-1.amazoncognito.com/login?client_id=651p7go9kkj4j3fr78qph3j96t&response_type=token&scope=email+openid+profile&redirect_uri=https://rp.dyslexia.ie";
+export const readable_cognito_logout_url = "https://readable.auth.eu-west-1.amazoncognito.com/login?client_id=651p7go9kkj4j3fr78qph3j96t&response_type=token&redirect_uri=https://rp.dyslexia.ie";
+export const api_url = "https://qihx5f0xfa.execute-api.eu-west-1.amazonaws.com/Stage"
+export const google_client_id = "925322795697-i9jr2o8gkqan3cdqvkn7tjog5la33av6.apps.googleusercontent.com"
+export const frontend_url = "https://rp.readable.ie"
