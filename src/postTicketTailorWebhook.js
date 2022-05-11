690a3dcf4d4c232b0a7c9f60ad7b6ac1d53f2e0d
aromakh <andriyromakh@gmail.com>
2022-05-11 17:00:27 +0300

    webhook full sync.

--- src/postTicketTailorWebhook.js
+++ src/postTicketTailorWebhook.js
@@ -30,32 +30,35 @@ exports.handler = async(event, context, callback) => {
         };
     }
 
-    if (data.event === 'ORDER.CREATED'){
-        var group = (await getGroupByEventId(data.payload.event_id)).Items[0];
-        
-        console.log(group);
+    if (data.event === 'Issued ticket' || data.event === 'ISSUED_TICKET.CREATED'){
 
-        if (!group){
-            return {
-                'statusCode': 200,
-                'body': 'ok',
-                'headers': {
-                    'Access-Control-Allow-Origin': '*',
-                }
-            };
+        var group = (await getGroupByEventId(data.payload.event_id)).Items[0];
+        var orders = (await getOrders(group.eventId)).data.data;
+
+        var participants = [];
+
+        for (var order of orders){
+            try {
+                if (order.issued_tickets[0].status === 'void') continue;
+
+                var participant = await createOrUpdateParticipant(order, group.id);
+                participants.push(participant)
+            } catch(e) {
+                console.log(e);
+            }
         }
 
         if (!group.participants)
             group.participants = [];
-    
-        var participant = await createOrUpdateParticipant(data.payload, group.id);
 
-        var existingParticipantIndex = group.participants.findIndex(r => r.id == participant.id);
+        for (var participant of participants){
+            var existingParticipantIndex = group.participants.findIndex(r => r.id == participant.id);
 
-        if (existingParticipantIndex === -1)
-            group.participants.push(participant);
-        else{
-            group.participants[existingParticipantIndex] = participant;
+            if (existingParticipantIndex === -1)
+                group.participants.push(participant);
+            else{
+                group.participants[existingParticipantIndex] = participant;
+            }
         }
 
         var params = {
@@ -63,9 +66,47 @@ exports.handler = async(event, context, callback) => {
             Item: group
         };
 
-        await dynamo.put(params).promise();
-
+        await dynamo.put(params).promise();    
     }
+
+
+
+    // if (data.event === 'ORDER.CREATED'){
+    //     var group = (await getGroupByEventId(data.payload.event_id)).Items[0];
+        
+    //     console.log(group);
+
+    //     if (!group){
+    //         return {
+    //             'statusCode': 200,
+    //             'body': 'ok',
+    //             'headers': {
+    //                 'Access-Control-Allow-Origin': '*',
+    //             }
+    //         };
+    //     }
+
+    //     if (!group.participants)
+    //         group.participants = [];
+    
+    //     var participant = await createOrUpdateParticipant(data.payload, group.id);
+
+    //     var existingParticipantIndex = group.participants.findIndex(r => r.id == participant.id);
+
+    //     if (existingParticipantIndex === -1)
+    //         group.participants.push(participant);
+    //     else{
+    //         group.participants[existingParticipantIndex] = participant;
+    //     }
+
+    //     var params = {
+    //         TableName: 'group',
+    //         Item: group
+    //     };
+
+    //     await dynamo.put(params).promise();
+
+    // }
 
     response = {
         'statusCode': 200,
@@ -91,11 +132,75 @@ const getGroupByEventId = async(eventId) => {
     return await dynamo.query(params).promise();
 };
 
+// const createOrUpdateParticipant = async (order, groupId) => {
+
+//     var participant = await getParticipant(order.id);
+
+//     participant.groupId = groupId;
+
+//     participant.parent_name = order.buyer_details.name;
+//     participant.type = order.issued_tickets[0].description;
+//     participant.created_at = order.issued_tickets[0].created_at;
+//     participant.county = order.buyer_details.custom_questions[0].answer;
+//     participant.child_name = order.buyer_details.custom_questions[2].answer;
+//     participant.class = order.buyer_details.custom_questions[3].answer;
+//     participant.email = order.buyer_details.email;
+//     participant.phone = order.buyer_details.phone;
+
+//     var params = {
+//         TableName: 'participant',
+//         Item: participant
+//     };
+
+//     await dynamo.put(params).promise();
+
+//     return participant;
+// }
+
+// const getParticipant = async(participantId) => {
+//     var participant = null;
+
+//     try{
+//         var params = {
+//             TableName: 'participant',
+//             Key: {
+//                 'id': participantId
+//             }
+//         };
+//         participant = await dynamo.get(params).promise();
+//     } catch(e){
+//         return null;
+//     }
+  
+//     return participant
+// };
+
+
+const getOrders = async(eventId) => {
+    const ticketTailorSk = await ssm.getParameter({
+        Name: process.env.TICKET_TAILOR_SK,
+        WithDecryption: true
+    }).promise();
+
+    return await axios.get(`https://api.tickettailor.com/v1/orders?event_id=${eventId}&status=completed`, 
+            {
+                auth:{
+                    username: ticketTailorSk.Parameter.Value,
+                    password: ""
+                }
+            }
+        );
+}
+
 const createOrUpdateParticipant = async (order, groupId) => {
+    var participant = (await getParticipant(order.id))?.Item;
 
-    var participant = await getParticipant(order.id);
-
-    participant.groupId = groupId;
+    if (!participant || !participant.id ){
+        participant = {
+            id: order.id,
+            groupId
+        }
+    } 
 
     participant.parent_name = order.buyer_details.name;
     participant.type = order.issued_tickets[0].description;
