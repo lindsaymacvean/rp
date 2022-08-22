2c6b819ba99849444e57b957870295259f5f101c
Lindsay MacVean <lindsaymacvean@gmail.com>
2022-08-22 11:54:06 +0100

    added groups to facilitator page

--- docs/scripts/facilitator.js
+++ docs/scripts/facilitator.js
@@ -16,17 +16,18 @@ globalThis.logout = logout;
             }
         })
         .then(resp => {
+            console.log(resp);
             var template = Handlebars.compile(document.querySelector("#facilitatorDataTempalte").innerHTML);
             document.querySelector("#facilitatorData").innerHTML = template(resp.data.Item);
         })
 
-    axios.get(`${api_url}/group/list?facilitatorId=${id}`, {
-            headers: {
-                'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
-            }
-        })
-        .then(resp => {
-            var template = Handlebars.compile(document.querySelector("#groupTemplate").innerHTML);
-            document.querySelector("#groupOutput").innerHTML = template({ groups: resp.data.Responses.group });
-        })
+    // axios.get(`${api_url}/group/list?facilitatorId=${id}`, {
+    //         headers: {
+    //             'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
+    //         }
+    //     })
+    //     .then(resp => {
+    //         var template = Handlebars.compile(document.querySelector("#groupTemplate").innerHTML);
+    //         document.querySelector("#groupOutput").innerHTML = template({ groups: resp.data.Responses.group });
+    //     })
 })();
--- lambda-app/src/getFacilitator.js
+++ lambda-app/src/getFacilitator.js
@@ -17,6 +17,42 @@ exports.handler = async(event, context) => {
 
         var body = await dynamo.get(params).promise();
 
+        // Get groups for facilitator
+        body.Item.groups = {};
+        for (var group in body.Item.groupsIds) {
+            var groupId = body.Item.groupsIds[group];
+            var groupParams = {
+                TableName: 'group',
+                Key: {
+                    'id': groupId
+                }
+            };
+            groupDetails = await dynamo.get(groupParams).promise();
+            if (groupDetails.Item) {
+                body.Item.groups[groupId] = groupDetails.Item;
+            }
+        }
+
+        // Get semester name for each group
+        for (const [key, value] of Object.entries(body.Item.groups)) {
+            // console.log("key, value", key, value);
+            // console.log(body.Item.groups[key]);
+            try {
+                var semesterId = body.Item.groups[key].semesterId;
+                var semesterParams = {
+                    TableName: 'semester',
+                    Key: {
+                        'id': semesterId
+                    }
+                }
+                var semesterDetails = await dynamo.get(semesterParams).promise();
+                console.log(body.Item.groups[key]);
+                console.log(semesterDetails);
+                body.Item.groups[key].semesterName = semesterDetails.Item.name;
+            } catch (e) {
+                console.log(e);
+            }
+        }
 
         response = {
             'statusCode': 200,
