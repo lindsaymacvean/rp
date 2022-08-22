217461fabf6c4c2b58ec4d32d9fc3fce776ab9c3
Lindsay MacVean <lindsaymacvean@gmail.com>
2022-08-22 23:15:42 +0100

    added improved update of facilitator on group

--- docs/scripts/facilitator.js
+++ docs/scripts/facilitator.js
@@ -20,14 +20,4 @@ globalThis.logout = logout;
             var template = Handlebars.compile(document.querySelector("#facilitatorDataTempalte").innerHTML);
             document.querySelector("#facilitatorData").innerHTML = template(resp.data.Item);
         })
-
-    // axios.get(`${api_url}/group/list?facilitatorId=${id}`, {
-    //         headers: {
-    //             'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
-    //         }
-    //     })
-    //     .then(resp => {
-    //         var template = Handlebars.compile(document.querySelector("#groupTemplate").innerHTML);
-    //         document.querySelector("#groupOutput").innerHTML = template({ groups: resp.data.Responses.group });
-    //     })
 })();
--- lambda-app/src/putGroupFacilitator.js
+++ lambda-app/src/putGroupFacilitator.js
@@ -11,21 +11,12 @@ exports.handler = async(event, context) => {
         
         // Get the group from the group table
         var data = JSON.parse(event.body);
+        // Update the facilitator before updating the Group
+        // Because UpdateFacilitator checks the group to find the current facilitator
+        await removeOldFacilitator(data);
+        await addNewFacilitator(data);
         await updateGroup(data);
-
-        var userDetails = event.requestContext.authorizer.claims;
-        var facilitatorChange = {
-            TableName: 'changelogs',
-            Item: {
-                id: crypto.randomUUID(),
-                date: new Date().toISOString(),
-                event: {
-                    user: userDetails,
-                    description: `${userDetails.name} set facilitator for group ${data.id} to facilitatorId ${data.facilitatorId}.`
-                }
-            }
-        }
-        await dynamo.put(facilitatorChange).promise();
+        await updateLogs(event, data);
 
         response = {
             'statusCode': 200,
@@ -58,3 +49,85 @@ const updateGroup = async(data) => {
     await dynamo.update(params).promise();
 
 };
+
+const removeOldFacilitator = async(data) => {
+    // Get the old facilitatorId
+    var groupParams = {
+        TableName: 'group',
+        Key: {
+            'id': data.id
+        }
+    }
+    var group = await dynamo.get(groupParams).promise();
+    var oldFacilitatorId = group.Item.facilitatorId;
+
+    // Get the list of groups associated with the old facilitator
+    var facilitatorParams = {
+        TableName: 'facilitator',
+        Key: {
+            'id': oldFacilitatorId
+        }
+    }
+    var facilitator = await dynamo.get(facilitatorParams).promise();
+    var groupList = facilitator.Item.groupsIds;
+
+    // Remove the group from the list of groups associated with the old facilitator
+    var groupIndex = groupList.findIndex(i => i === data.id);
+    // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.UpdateExpressions.html
+    var UpdateExpression = 'REMOVE groupsIds['+groupIndex+']';
+    var removeGroupParams = {
+        TableName: 'facilitator',
+        Key: {
+            'id': oldFacilitatorId
+        },
+        UpdateExpression
+    }
+
+    try {
+        await dynamo.update(removeGroupParams).promise();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const addNewFacilitator = async(data) => {
+    // Add group to new facilitator
+    var ExpressionAttributeNames = {
+        '#ri': 'groupsIds'
+    };
+    var ExpressionAttributeValues = {
+        ':vals': [data.id]
+    };
+    var UpdateExpression = 'SET #ri = list_append(#ri, :vals)';
+    var addGroupParams = {
+        TableName: 'facilitator',
+        Key: {
+            'id': data.facilitatorId
+        },
+        UpdateExpression,
+        ExpressionAttributeNames,
+        ExpressionAttributeValues
+    }
+
+    try {
+        await dynamo.update(addGroupParams).promise();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const updateLogs = async(event, data) => {
+    var userDetails = event.requestContext.authorizer.claims;
+    var facilitatorChange = {
+        TableName: 'changelogs',
+        Item: {
+            id: crypto.randomUUID(),
+            date: new Date().toISOString(),
+            event: {
+                user: userDetails,
+                description: `${userDetails.name} set facilitator for group ${data.id} to facilitatorId ${data.facilitatorId}.`
+            }
+        }
+    }
+    await dynamo.put(facilitatorChange).promise();
+}
