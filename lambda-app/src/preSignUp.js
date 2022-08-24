d62b68d39a880c5ce77ccaa8db629e0e0695678a
Lindsay MacVean <lindsaymacvean@gmail.com>
2022-08-24 02:32:15 +0100

    added comment with some documentation for authorizer

--- lambda-app/src/preSignUp.js
+++ lambda-app/src/preSignUp.js
@@ -1,3 +1,4 @@
+// https://aws.amazon.com/blogs/security/how-to-secure-api-gateway-http-endpoints-with-jwt-authorizer/
 const AWS = require('aws-sdk');
 
 exports.handler = async(event, context, callback) => {
