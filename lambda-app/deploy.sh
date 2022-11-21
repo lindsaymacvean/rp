49bb4f9125ba73589f32d3194c48caae3d91b976
Lindsay MacVean <lindsaymacvean@gmail.com>
2022-11-21 21:17:22 +0000

    added csvwriterlayer

--- lambda-app/deploy.sh
+++ lambda-app/deploy.sh
@@ -5,5 +5,8 @@ cd -
 cd layers/axiosLayer/nodejs
 npm install
 cd -
+cd layers/CSVWriterLayer
+npm install
+cd -
 sam package --template-file template.yml --output-template-file package.yml --s3-bucket dyslexia-readable-depl-bucket --region eu-west-1 --profile dyslexia
 sam deploy --template-file package.yml --stack-name readable-stage-stack --capabilities CAPABILITY_IAM --region eu-west-1 --tags ReadableProject=1 --profile dyslexia
