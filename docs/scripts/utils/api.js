c9f5131d30665d586f2f41d962ad53bd034c2719
Lindsay MacVean <lindsaymacvean@gmail.com>
2022-11-21 21:08:20 +0000

    updated api.js to have promise.resolve for all calls so that then works when calling the methods

--- docs/scripts/utils/api.js
+++ docs/scripts/utils/api.js
@@ -1,66 +1,107 @@
 import { api_url } from "./configs.js"
 
-export const getSemester = (semesterId) => {
+export const getSemester = async (semesterId) => {
    if (typeof semesterId === 'undefined' || semesterId === null ) return;
-   return axios.get(`${api_url}/semester?id=${semesterId}`, {
-        headers: {
-            'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
-        }
-    })
+   let response = Promise.resolve();
+   try {
+    response = await axios.get(`${api_url}/semester?id=${semesterId}`, {
+      headers: {
+          'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
+      }
+    });
+   } catch(e) {
+    console.log(e);
+   }
+   return response;
 }
 
 export const getGroup = async (groupId) => {
     if (typeof groupId === 'undefined' || groupId === null) return new Error('There is no groupId key.');
-    return await axios.get(`${api_url}/group?id=${groupId}`, {
+    let response = Promise.resolve();
+    try {
+      response = await axios.get(`${api_url}/group?id=${groupId}`, {
         headers: {
             'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
         }
-    })
+      })
+    } catch(e) {
+      console.log(e);
+    }
+    return response;
 }
 
 export const getParticipants = async (groupId) => {
     if (typeof groupId === 'undefined' || groupId === null) return new Error('There is no groupId key.');
-    return await axios.get(`${api_url}/participants?id=${groupId}`, {
+    let response = Promise.resolve();
+    try {
+      response = await axios.get(`${api_url}/participants?id=${groupId}`, {
+        headers: {
+          'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
+        }
+      });
+    } catch(e) {
+      console.log(e);
+    }
+    return response;
+}
+
+export const getSemesterGroupList = async (semesterId) => {
+    if (typeof semesterId === 'undefined' || semesterId === null) return {};
+    let response = Promise.resolve();
+    try {
+      response = await axios.get(`${api_url}/group/semester?semesterId=${semesterId}`, {
+        headers: {
+            'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
+        }
+      });
+    } catch(e) {
+      console.log(e);
+    }
+    return response;
+}
+
+
+export const getFacilitator = async (facilitatorId) => {
+    if (typeof facilitatorId === 'undefined' || facilitatorId === null) return;
+    let response = Promise.resolve();
+    try {
+      response = await axios.get(`${api_url}/facilitator?id=${facilitatorId}`, {
+        headers: {
+            'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
+        }
+      })
+    } catch(e) {
+      console.log(e);
+    }
+    return response;
+ }
+
+export const getFacilitators = async () => {
+  let response = Promise.resolve();
+  try {
+    response = await axios.get(`${api_url}/facilitator/list`, {
+      headers: {
+        'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
+      }
+    })
+  } catch(e) {
+    console.log(e);
+  }
+  return response.data.Items;
+}
+
+export async function getStats(semesterId) {
+  let response = Promise.resolve();
+  try {
+    response = await axios.get(`${api_url}/stats?semesterid=${semesterId}`, {
       headers: {
         'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
       }
     });
-}
-
-export const getSemesterGroupList = (semesterId) => {
-    if (typeof semesterId === 'undefined' || semesterId === null) return;
-    return axios.get(`${api_url}/group/semester?semesterId=${semesterId}`, {
-        headers: {
-            'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
-        }
-    });
-}
-
-
-export const getFacilitator = (facilitatorId) => {
-    if (typeof facilitatorId === 'undefined' || facilitatorId === null) return;
-    return axios.get(`${api_url}/facilitator?id=${facilitatorId}`, {
-       headers: {
-           'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
-       }
-   })
- }
-
-export const getFacilitators = async () => {
-  const { data } = await axios.get(`${api_url}/facilitator/list`, {
-    headers: {
-      'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
-    }
-  })
-  return data.Items;
-}
-
-export async function getStats(semesterId) {
-  return axios.get(`${api_url}/stats?semesterid=${semesterId}`, {
-    headers: {
-      'Authorization': `Bearer ${sessionStorage.getItem('id_token')}`
-    }
-  });
+  } catch(e) {
+    console.log(e);
+  }
+  return response;
 }
 
 
