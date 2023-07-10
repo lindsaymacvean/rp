2d856c8b6f1a31a967a3e984670878c3a1bf5138
Lindsay MacVean <lindsaymacvean@gmail.com>
2023-07-10 13:56:52 +0100

    alphabetical order of facilitators

--- docs/scripts/facilitators_list.js
+++ docs/scripts/facilitators_list.js
@@ -10,6 +10,10 @@ import { IsLoggedIn } from "./utils/isLoggedIn.js";
             }
         })
         .then(resp => {
+            console.log(resp.data.Items);
+            resp.data.Items.sort((a,b)=> {
+                return (a.given_name+a.family_name).localeCompare((b.given_name+b.family_name));
+            })
             var template = Handlebars.compile(document.querySelector("#facilitatorTemplate").innerHTML);
             document.querySelector("#facilitatorsList").innerHTML = template({ facilitators: resp.data.Items });
         })
